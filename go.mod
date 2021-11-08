0c3ee414ff0fa4f453a7397dfbe101ea7596a9e2
Matheus Moraes <matheusfaria.moraes@gmail.com>
2021-11-08 17:43:28 -0300

    implements players client

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/matheusfm/futbin
 
 go 1.17
+
+require github.com/google/go-querystring v1.1.0
--- players/client.go
+++ players/client.go
@@ -1,6 +1,12 @@
 package players
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
 
 type Players interface {
 	Get(opt PlayerOptions) ([]Player, error)
@@ -11,5 +17,20 @@ type players struct {
 }
 
 func (p players) Get(opt PlayerOptions) ([]Player, error) {
-	panic("implement me")
+	u, _ := url.Parse("https://www.futbin.org/futbin/api/getFilteredPlayers")
+	q, err := query.Values(opt)
+	if err != nil {
+		return nil, err
+	}
+	u.RawQuery = q.Encode()
+	r, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	var data playersData
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data.Players, err
 }
